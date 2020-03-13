a06898633fff1cbbff6b73f963d58df72f5987aa
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2020-03-13 19:09:29 +0100

    Added API Description and gitignore

--- target/generated-sources/annotations/entities/Hobby_.java
+++ target/generated-sources/annotations/entities/Hobby_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-13T17:20:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-03-13T18:25:14")
 @StaticMetamodel(Hobby.class)
 public class Hobby_ { 
 
