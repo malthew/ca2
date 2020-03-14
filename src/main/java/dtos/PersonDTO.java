c98f32966962650b057ff5de83f1591ce458369a
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2020-03-14 12:34:51 +0100

    Added PersonDTO and PersonFacade

---
+++ src/main/java/dtos/PersonDTO.java
@@ -0,0 +1,74 @@
+package dtos;
+
+import entities.Address;
+import entities.Hobby;
+import entities.Person;
+import entities.Phone;
+import java.util.List;
+
+public class PersonDTO {
+    private String email;
+    private String firstName;
+    private String lastName;
+    private List<Phone> phones;
+    private Address address;
+    private List<Hobby> hobbies;
+
+    public PersonDTO(Person person) {
+        this.email = person.getEmail();
+        this.firstName = person.getFirstName();
+        this.lastName = person.getLastName();
+        this.phones = person.getPhones();
+        this.address = person.getAddress();
+        this.hobbies = person.getHobbys();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public List<Hobby> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<Hobby> hobbies) {
+        this.hobbies = hobbies;
+    }
+    
+}
